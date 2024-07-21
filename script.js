58db2e028a17af0e915aaac76c2ab7757a8c7957
VijayKumarKalla <vijaykalla96@gmail.com>
2024-07-21 13:09:47 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+function appear() {
+    var f = document.getElementById("footer");
+    var k = document.getElementById("career");
+    var g = document.getElementById("aboutus");
+    var h = document.getElementById("services3");
+    var i = document.getElementById("services");
+    var j = document.getElementById("services2");
+    var l = document.getElementById("services4");
+    var m = document.getElementById("career2");
+    var n = document.getElementById("carouselExampleIndicators");
+    var o = document.getElementById("career3");
+    f.style.display = "";
+    k.style.display = "none";
+    g.style.display = "none";
+    h.style.display = "";
+    i.style.display = "";
+    j.style.display = "";
+    l.style.display = "";
+    m.style.display = "none";
+    n.style.display = "none";
+    o.style.display = "none";
+}
+
+function hide() {
+    var a = document.getElementById("footer");
+    var b = document.getElementById("career");
+    var c = document.getElementById("aboutus");
+    var d = document.getElementById("services3");
+    var e = document.getElementById("services");
+    var p = document.getElementById("services2");
+    var q = document.getElementById("services4");
+    var r = document.getElementById("career2");
+    var s = document.getElementById("carouselExampleIndicators");
+    var t = document.getElementById("career3");
+    a.style.display = "";
+    b.style.display = "";
+    c.style.display = "";
+    d.style.display = "none";
+    e.style.display = "none";
+    p.style.display = "none";
+    q.style.display = "none";
+    r.style.display = "";
+    s.style.display = "";
+    t.style.display = "";
+}
